cc1980da89d5d81885c438f70431c136cdb35b61
UberRipper <uberripper@gmail.com>
2022-11-04 18:42:48 +0000

    added find all at the server and pagination with page ==15

--- responses.py
+++ responses.py
@@ -1,8 +1,48 @@
 import json
 from urllib.request import urlopen
+from table2ascii import table2ascii as t2a, PresetStyle
+
+PAGE_LIMIT = 15
 
 
-def callApi(param: str) -> str:
+def convert_to_table(data_json: json, start=0) -> str:
+
+    body = []
+
+    total_count = 0
+    count = 0
+    for obj in data_json:
+
+        total_count += 1
+
+        if total_count > start:
+
+            if count > PAGE_LIMIT:
+                break
+            count += 1
+
+            body.append([
+                        total_count-1,
+                        obj["id"],
+                        obj["name"],
+                        obj["qty"],
+                        obj["location"]["locX"],
+                        obj["location"]["locY"]])
+    output = t2a(
+        header=["row",
+                "id",
+                "name",
+                "qty",
+                "x",
+                "y"],
+        body=body,
+        first_col_heading=True
+    )
+
+    return f"```\nItems from {start} to {start+PAGE_LIMIT} for server <{data_json[0]['location']['locAddress']}>\n\n{output}\n```"
+
+
+def callApi(param: str, start=0) -> str:
 
     # import json
 
@@ -20,7 +60,7 @@ def callApi(param: str) -> str:
 
     data_json = json.loads(response.read())
 
-    return str(data_json)
+    return convert_to_table(data_json, start=start)
 
 
 def get_response(message: str) -> str:
@@ -28,11 +68,16 @@ def get_response(message: str) -> str:
 
     if 'getGetAllItems' in p_message and p_message.split(" ")[0] == 'getGetAllItems':
         print(p_message)
-        if p_message.split(" ")[1] != "":
-            return callApi('/getAllAtLocation?address=' + p_message.split(" ")[1])
+        address = p_message.split(" ")[1]
+        if address != "":
+            api_call = '/getAllAtLocation?address=' + address
+            start_value = 0
+            if len(p_message.split("start=")) == 2:
+                start_value = int(p_message.split("start=")[1].split(" ")[0])
+            return callApi(api_call, start=start_value)
         return 'Please enter location too'
 
     if p_message.lower() == '!help':
-        return '`1. getGetAllItems <location>`'
+        return '`1. getGetAllItems <location> start=<start optional>`'
 
     return 'I didn\'t understand what you wrote. Try typing "!help".'
