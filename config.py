6b7241f9098a26ff8474787df5a82aa018b11482
UberRipper <uberripper@gmail.com>
2022-11-05 22:34:06 +0000

    security implemented

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 URL = "http://45.63.39.115:8080"
+ALLOWED_USERNAMES = [
+    "UberRipper#9053",
+    "chezter009#6714"
+]
+
+ALLOWED_CHANNELS = [
+    "shit"
+]
+
+PREFIX = "shitbot"
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import responses_read
 import responses_file
+import config
 
 
 async def send_message(message, user_message, is_private):
@@ -33,17 +34,29 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" ({channel})')
-
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            if "addFile" in user_message:
-                await message.channel.send(responses_file.add_file_to_db(
-                    message, user_message, message.attachments[0].url))
+        if user_message.split(" ")[0] == config.PREFIX:
+            print(user_message)
+            user_message = user_message.removeprefix(config.PREFIX).strip()
+            print(user_message)
+            if not (username in config.ALLOWED_USERNAMES):
+                print(username + " tried and had no access")
+                await message.channel.send(username + " tried and had no access")
+            elif not (channel in config.ALLOWED_CHANNELS):
+                print(f"wrong channel used user {username} tried {channel}")
+                await message.channel.send(
+                    f"wrong channel used user {username} tried {channel}")
             else:
-                await send_message(message, user_message, is_private=False)
+                print(f'{username} said: "{user_message}" ({channel})')
+
+                if user_message[0] == '?':
+                    user_message = user_message[1:]
+                    await send_message(message, user_message, is_private=True)
+                else:
+                    if "addFile" in user_message:
+                        await message.channel.send(responses_file.add_file_to_db(
+                            message, user_message, message.attachments[0].url))
+                    else:
+                        await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
 
