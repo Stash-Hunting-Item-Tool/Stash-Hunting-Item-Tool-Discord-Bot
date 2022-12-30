42b0c2d11699ef5c0bd1b3778aa9363a9e2251ea
M100K300 <96502613+M100K300@users.noreply.github.com>
2022-12-30 00:51:02 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 URL = "http://45.63.39.115:8080"
 ALLOWED_USERNAMES = [
     "UberRipper#9053",
-    "chezter009#6714"
+    "chezter009#6714",
+    "Konax#9035",
+    "BlankSlated#2753",
+    "slowsnail12#8603"
 ]
 
 FULL_ACCESS = [
     "UberRipper#9053",
-    "chezter009#6714"
+    "chezter009#6714",
+    "Konax#9035",
+    "BlankSlated#2753",
+    "slowsnail12#8603"
 ]
 PARTIAL_ACCESS = [
 
