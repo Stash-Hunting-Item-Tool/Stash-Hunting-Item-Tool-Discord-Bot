db8051c8aac1b91c27d88e887ca58a62ddef44ea
M100K300 <96502613+M100K300@users.noreply.github.com>
2023-09-21 16:13:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,18 @@
 URL = "http://45.63.39.115:8080"
 ALLOWED_USERNAMES = [
-    "UberRipper#9053",
-    "chezter009#6714",
-    "Konax#9035",
-    "BlankSlated#2753",
-    "slowsnail12#8603"
+    "user",
+    "user"
 ]
 
 FULL_ACCESS = [
-    "UberRipper#9053",
-    "chezter009#6714",
-    "Konax#9035",
-    "BlankSlated#2753",
-    "slowsnail12#8603"
+    "user"
 ]
 PARTIAL_ACCESS = [
 
 ]
 
 ALLOWED_CHANNELS = [
-    "shit"
+    "channel_name"
 ]
 
 PREFIX = "shitbot"
